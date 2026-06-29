0fe007df5a10fba9b0f2c5f194cd69844a41ea1e
akeysru-retro <akeysru@gmail.com>
2026-06-29 18:14:10 +0700

    Create games.js

---
+++ games.js
@@ -0,0 +1,6 @@
+window.MY_GAMES = [
+    { file: "mario.nes", title: "SUPER MARIO BROS" },
+    { file: "tanks.nes", title: "TANK 1990 (BATTLE CITY)" },
+    { file: "chip.nes", title: "CHIP N DALE" },
+    { file: "contra.nes", title: "CONTRA" }
+];
