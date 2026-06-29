b83673878ae6ef9868495238166e1ff694e2891f
akeysru-retro <akeysru@gmail.com>
2026-06-29 20:22:31 +0700

    Update games.js

--- games.js
+++ games.js
@@ -2,8 +2,9 @@ window.MY_GAMES = [
     { file: "mario.nes", title: "SUPER MARIO BROS", system: "nes" },
     { file: "tanks.nes", title: "TANK 1990 (BATTLE CITY)", system: "nes" },
     { file: "chip.nes", title: "CHIP N DALE", system: "nes" },
-	{ file: "felix.nes", title: "Felix the Cat" , system: "nes" },}
+    { file: "felix.nes", title: "FELIX THE CAT", system: "nes" },
     { file: "contra.nes", title: "CONTRA", system: "nes" },
     { file: "sonic.gen", title: "SONIC THE HEDGEHOG", system: "sega" },
-    { file: "mk3.gen", title: "MORTAL KOMBAT 3 ULTIMATE", system: "sega" }
+    { file: "mk3.gen", title: "MORTAL KOMBAT 3 ULTIMATE", system: "sega" },
+    { file: "smw.sfc", title: "SUPER MARIO WORLD", system: "snes" } // Пример для SNES
 ];
