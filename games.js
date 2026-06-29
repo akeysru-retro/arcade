68d8775fb115cb19b335d1133663e798a66ffbbb
akeysru-retro <akeysru@gmail.com>
2026-06-29 18:31:50 +0700

    Update games.js

--- games.js
+++ games.js
@@ -2,5 +2,6 @@ window.MY_GAMES = [
     { file: "mario.nes", title: "SUPER MARIO BROS" },
     { file: "tanks.nes", title: "TANK 1990 (BATTLE CITY)" },
     { file: "chip.nes", title: "CHIP N DALE" },
-    { file: "contra.nes", title: "CONTRA" }
+    { file: "contra.nes", title: "CONTRA" },
+    { file: "felix.nes", title: "Felix the Cat" }
 ];
