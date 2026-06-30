63dba605c5335adcb0bbcec4cc03bdc81194950c
akeysru-retro <akeysru@gmail.com>
2026-06-30 14:04:56 +0700

    Update games.js

--- games.js
+++ games.js
@@ -1,9 +1,18 @@
 window.MY_GAMES = [
+    // --- NES / DENDY ---
     { file: "mario.nes", title: "SUPER MARIO BROS", system: "nes" },
     { file: "tanks.nes", title: "TANK 1990 (BATTLE CITY)", system: "nes" },
     { file: "chip.nes", title: "CHIP N DALE", system: "nes" },
-    { file: "felix.nes", title: "FELIX THE CAT" , system: "nes" },
+    { file: "felix.nes", title: "FELIX THE CAT", system: "nes" },
     { file: "contra.nes", title: "CONTRA", system: "nes" },
+
+    // --- SEGA MEGA DRIVE ---
     { file: "sonic.gen", title: "SONIC THE HEDGEHOG", system: "sega" },
-    { file: "mk3.gen", title: "MORTAL KOMBAT 3 ULTIMATE", system: "sega" }
+    { file: "mk3.gen", title: "MORTAL KOMBAT 3 ULTIMATE", system: "sega" },
+
+    // --- SNES (Super Nintendo) ---
+    { file: "smw.sfc", title: "SUPER MARIO WORLD", system: "snes" },
+
+    // --- GBA (Game Boy Advance) ---
+    { file: "pokemon.gba", title: "POKEMON FIRE RED", system: "gba" }
 ];
