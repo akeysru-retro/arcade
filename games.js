98cd21a04b0acb9d21b43c956dd7b91f0d89c87c
akeysru-retro <akeysru@gmail.com>
2026-06-30 16:33:44 +0700

    Update games.js

--- games.js
+++ games.js
@@ -7,3 +7,11 @@ window.MY_GAMES = [
     { file: "sonic.gen", title: "SONIC THE HEDGEHOG", system: "sega" },
     { file: "mk3.gen", title: "MORTAL KOMBAT 3 ULTIMATE", system: "sega" }
 ];
+
+// --- ВЫДЕЛЕННЫЙ МАССИВ ДЛЯ СЕКРЕТОВ И ЧИТ-КОДОВ ---
+window.GAME_SECRETS = [
+    { game: "Mortal Kombat 3", type: "Sega", title: "Меню читов (Cool Stuff)", code: "В главном меню нажмите: C, Вправо, A, Влево, A, Вверх" },
+    { game: "Contra", type: "NES", title: "30 жизней", code: "На заставке: Вверх, Вверх, Вниз, Вниз, Влево, Вправо, Влево, Вправо, B, A, START" },
+    { game: "Sonic The Hedgehog", type: "Sega", title: "Выбор уровней", code: "На заставке нажмите: Вверх, Вниз, Влево, Вправо. Зажмите A и нажмите START" },
+    { game: "Battle City (Танчики)", type: "NES", title: "Выбор уровня", code: "В главном меню наведите на 1 Player и нажимайте Вправо или Влево" }
+];
